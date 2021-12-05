0fb6b69bf908a6a629369082e290b9e60f9fc662
zhouzhipeng <admin@zhouzhipeng.com>
2021-12-05 11:56:38 +0800

    update

--- js/custom.js
+++ js/custom.js
@@ -76,7 +76,7 @@ $(document).ready(function () {
     AOS.init({
         duration: 1200,
         once: true,
-        disable: 'mobile'
+        // disable: 'mobile'
     });
 
     //  isotope
@@ -140,10 +140,11 @@ $(document).ready(function () {
                 }
             },
             submitHandler: function (form) {
+                console.log($(form).serialize())
                 $(form).ajaxSubmit({
                     type: "POST",
-                    data: $(form).serialize(),
-                    url: "process.php",
+                    // data: $(form).serialize(),
+                    url: "https://api.zhouzhipeng.com/contact-me",
                     success: function () {
                         $('#contact :input').attr('disabled', 'disabled');
                         $('#contact').fadeTo("slow", 1, function () {
